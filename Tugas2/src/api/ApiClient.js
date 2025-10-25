117b9ae65f26d06b0cbd0fbae1815e636c7e0947
Michael Hermanto <michhu1212@gmail.com>
2025-10-25 14:46:10 +0700

    hm

--- Tugas2/src/api/ApiClient.js
+++ Tugas2/src/api/ApiClient.js
@@ -1,13 +1,8 @@
-import axios from "axios";
+import axios from 'axios'
 
-// Membuat instance Axios dengan konfigurasi default
-const apiClient = axios.create({
-    // URL dasar untuk semua permintaan ke API kita
-    baseURL: 'http://127.0.0.1:8000', 
-    // Header default yang akan dikirim pada setiap permintaan
-    headers: {
-        "Accept": "application/json"
-    }
-});
 
-export default apiClient;
+const http = axios.create({
+    baseURL : "http://localhost"
+}) 
+
+export default http
